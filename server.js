b2e03e41c3b1a16896ca9b125b2f5373d720e3aa
Alexander Grimes <66704965+Alex-Grimes@users.noreply.github.com>
2022-02-25 14:44:25 -0500

    continued development

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const articleRouter = require('./routes/articles')
+
+
+mongoose.connect('mongodb;//localhost/blog', {
+    useNewUrlParser: true, useUnifiedTopology: true
+})
 
 app.set('view engine', 'ejs')
 
 app.use('/articles', articleRouter)
 
 app.get('/', (req, res) => {
-    res.render('index')
+    const articles = [{
+        title: 'Test Article',
+        createdAt: new Date(),
+        description: 'Test Description'
+    },
+    {
+    title: 'Test Article2',
+    createdAt: new Date(),
+    description: 'Test Description2'
+    }]
+
+    
+
+
+    res.render('index', { articles: articles })
 })
 app.listen(5005)
 
